0b004454c21e93ee3669cd554b2ca48f37f8ba3e
itsrusty <rusty3031@gmail.com>
2023-06-13 18:06:41 -0700

    new: añadi las rutas para añadir archivos

--- Api/server/index.js
+++ Api/server/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors"
+import register from "../routes/auth/register.auth.js"
+import login from "../routes/auth/login.auth.js"
+import upload_file from "../routes/files/upload.file.js"
+import upload_asset from "../routes/files/upload.asset.js"
 
 // initialization 
 const app = express();
@@ -13,6 +17,10 @@ app.use(cors())
 app.use(express.json())
 
 // routes
+app.use("/auth/user", register)
+app.use("/auth/user", login)
+app.use("/upload/files", upload_file)
+app.use("/upload/asset", upload_asset)
 
 // PORT
 const PORT = process.env.PORT || 3000;
