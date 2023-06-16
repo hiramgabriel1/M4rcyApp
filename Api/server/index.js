09b1c11f3dc1ad5ea0e5115239c3c03bf2fbd559
itsrusty <rusty3031@gmail.com>
2023-06-16 02:59:18 -0700

    finish: añadi el endpoint para los posts

--- Api/server/index.js
+++ Api/server/index.js
@@ -1,18 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import colors from "colors";
+import sequelize from "../db/conex.js";
 import register from "../routes/auth/register.auth.js";
 import login from "../routes/auth/login.auth.js";
+import posts from "../routes/users/posts.routes.js";
 import upload_file from "../routes/files/upload.file.js";
 import upload_asset from "../routes/files/upload.asset.js";
 import index from "../routes/index.routes.js";
 import filter_content from "../routes/search/filter.data.search.js";
-import filter_country from "../routes/search/filter.country.search.js"
-import comments from "../routes/users/users.comments.js"
-import sequelize from "../db/conex.js";
-import colors from "colors";
-
-// admin
+import filter_country from "../routes/search/filter.country.search.js";
+import comments from "../routes/users/users.comments.js";
 import report_create from "../routes/admin/add.report.js";
 import report_show from "../routes/admin/showReports.js";
 
@@ -20,7 +19,7 @@ import report_show from "../routes/admin/showReports.js";
 async () => {
   try {
     await sequelize.authenticate();
-    await sequelize.sync()
+    await sequelize.sync();
     console.log("autenticado!".rainbow);
   } catch (error) {
     throw new Error(error);
@@ -29,9 +28,6 @@ async () => {
 
 // initialization
 const app = express();
-
-// aqui se guarda el contenido guardado por los usuarios
-const dataContent = "../../content-file-user";
 
 // env configuration
 dotenv.config();
@@ -46,12 +42,15 @@ app.use("/auth/user", login);
 app.use("/upload/files", upload_file);
 app.use("/upload/asset", upload_asset);
 app.use("/search", filter_content);
-app.use("/search/country", filter_country )
-app.use("/add/comment", comments)
+app.use("/search/country", filter_country);
+app.use("/add/comment", comments);
 
 // ? routes admin
 app.use("/admin/report", report_create);
 app.use("/admin/report", report_show);
+
+// users
+app.use("/user", posts);
 
 // * index route
 app.use("/inicio", index);
